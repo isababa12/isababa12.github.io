bf139d4dceed2d2f9a459edf863fcbe8138c5782
Isaiah <isababa12@gmail.com>
2023-03-31 17:53:07 -0700

    adjusted logo to not depend on gsap

--- src/components/Contact/Logo/logo.js
+++ src/components/Contact/Logo/logo.js
@@ -1,38 +1,11 @@
 import './logo.scss';
 import LogoS from '../../../assets/images/logo-s.png'
-import { useEffect, useRef } from 'react';
-import { gsap } from 'gsap-trial';
 
 const Logo = () => {
 
-    const bgRef = useRef();
-    const outlineLogoRef = useRef();
-    const solidLogoRef=useRef()
-
-    useEffect(() => {
-
-        gsap
-            .timeline()
-            .to(bgRef.current, {
-                duration: 1,
-                opacity: 1,
-            })
-            gsap.fromTo(
-                solidLogoRef.current,
-                {
-                    opacity: 0,
-                },
-                {
-                    opacity: 1,
-                    delay:3,
-                    duration: 2,
-                }
-            )
-    }, [])
-
     return (
-        <div className='logo-container' ref={bgRef}>
-            <img ref={solidLogoRef} className='solid-logo' src={LogoS}/>
+        <div className='logo-container'>
+            <img className='solid-logo' src={LogoS}/>
         </div>
     )
 }
